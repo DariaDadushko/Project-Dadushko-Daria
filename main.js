99fcab75d1bb9c95c56c74cb3d246ee093edca15
DadiaDadushko <dariadadushko@gmail.com>
2021-04-28 09:58:47 +0300

    change data structure

--- main.js
+++ main.js
@@ -1,57 +1,110 @@
+const filmTitle = [
+  "The Shawshank Redemption",
+  "Schindler's List",
+  "Fight Club",
+  "Interstellar",
+  "Eternal Sunshine of the Spotless Mind",
+  "12 Years a Slave",
+];
+
+const filmPlot = [
+  "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+  "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+  "An insomniac unnamed narrator needs a fantasy to escape from his deadly boring life, he tries joining a cancer support group however the only thing they do in the group is cry into each others chest, but then he is on a plane on his way back from what a viewer would assume is a business trip our unnamed narrator encounters Tyler Durden, a soap selling bad-ass who happens to run a secret fight club in the diner parking lot with his friend who follows 8 simple rules set out by Tyler, our unnamed narrator of course is taken into this scheme ran by Tyler.",
+  "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  "When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories.",
+  "In the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
+];
+
+const filmPoster = [
+  "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY1200_CR89,0,630,1200_AL_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/515qyYBSIFL._AC_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/5190cUTcbZL._AC_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/81kz06oSUeL._AC_SL1500_.jpg",
+  "https://m.media-amazon.com/images/I/51dPIsyVaxL._AC_.jpg",
+  "https://m.media-amazon.com/images/I/61QFAKXbb5L._AC_SY879_.jpg",
+];
+
+const filmDirector = [
+  "Frank Darabont",
+  "Steven Spielberg",
+  "David Fincher",
+  "Christopher Nolan",
+  "Michel Gondry",
+  "Steve McQueen",
+];
+
+function generateRandomNumber() {}
+
+function generateRandomRating() {}
+
+function generateRandomReleseDate() {}
+
+function generateRandomBoxOffice() {}
+
 const filmList = [
-    {
-       title: 'The Shawshank Redemption',
-       releseDate: ' 14 October 1994',
-       plot: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-       poster: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY1200_CR89,0,630,1200_AL_.jpg',
-       boxOffice: '$25,000,000',
-       rating: 9.3,
-       director: 'Frank Darabont',   
-    },
-    {
-       title: 'Schindler\'s List',
-       releseDate: ' 30 November 1993 ',
-       plot: 'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
-       poster: 'https://images-na.ssl-images-amazon.com/images/I/515qyYBSIFL._AC_.jpg',
-       boxOffice: '$22,000,000',
-       rating: 8.9,
-       director: 'Steven Spielberg',  
-    },
-    {
-       title: 'Fight Club',
-       releseDate: ' 10 September 1999 ',
-       plot: 'An insomniac unnamed narrator needs a fantasy to escape from his deadly boring life, he tries joining a cancer support group however the only thing they do in the group is cry into each others chest, but then he is on a plane on his way back from what a viewer would assume is a business trip our unnamed narrator encounters Tyler Durden, a soap selling bad-ass who happens to run a secret fight club in the diner parking lot with his friend who follows 8 simple rules set out by Tyler, our unnamed narrator of course is taken into this scheme ran by Tyler.',
-       poster: 'https://images-na.ssl-images-amazon.com/images/I/5190cUTcbZL._AC_.jpg',
-       boxOffice: '$63,000,000',
-       rating: 8.8,
-       director: ' David Fincher',     
-    },
-    {
-       title: 'Interstellar',
-       releseDate: ' 26 October 2014 ',
-       plot: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
-       poster: 'https://images-na.ssl-images-amazon.com/images/I/81kz06oSUeL._AC_SL1500_.jpg',
-       boxOffice: '$165,000,000',
-       rating: 8.6,
-       director: '  Christopher Nolan',    
-    },
-    {
-       title: 'Eternal Sunshine of the Spotless Mind',
-       releseDate: ' 9 March 2004 ',
-       plot: 'When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories.',
-       poster: 'https://m.media-amazon.com/images/I/51dPIsyVaxL._AC_.jpg',
-       boxOffice: '$20,000,000',
-       rating: 8.3,
-       director: 'Michel Gondry',    
-    },
-    {
-       title: '12 Years a Slave',
-       releseDate: '  12 December 2013  ',
-       plot: 'In the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.',
-       poster: 'https://m.media-amazon.com/images/I/61QFAKXbb5L._AC_SY879_.jpg',
-       boxOffice: '$20,000,000',
-       rating: 8.1,
-       director: 'Steve McQueen',   
-    },
-    
-]
+  {
+    title: "The Shawshank Redemption",
+    releseDate: " 14 October 1994",
+    plot:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY1200_CR89,0,630,1200_AL_.jpg",
+    boxOffice: "$25,000,000",
+    rating: 9.3,
+    director: "Frank Darabont",
+  },
+  {
+    title: "Schindler's List",
+    releseDate: " 30 November 1993 ",
+    plot:
+      "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/I/515qyYBSIFL._AC_.jpg",
+    boxOffice: "$22,000,000",
+    rating: 8.9,
+    director: "Steven Spielberg",
+  },
+  {
+    title: "Fight Club",
+    releseDate: " 10 September 1999 ",
+    plot:
+      "An insomniac unnamed narrator needs a fantasy to escape from his deadly boring life, he tries joining a cancer support group however the only thing they do in the group is cry into each others chest, but then he is on a plane on his way back from what a viewer would assume is a business trip our unnamed narrator encounters Tyler Durden, a soap selling bad-ass who happens to run a secret fight club in the diner parking lot with his friend who follows 8 simple rules set out by Tyler, our unnamed narrator of course is taken into this scheme ran by Tyler.",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/I/5190cUTcbZL._AC_.jpg",
+    boxOffice: "$63,000,000",
+    rating: 8.8,
+    director: " David Fincher",
+  },
+  {
+    title: "Interstellar",
+    releseDate: " 26 October 2014 ",
+    plot:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/I/81kz06oSUeL._AC_SL1500_.jpg",
+    boxOffice: "$165,000,000",
+    rating: 8.6,
+    director: "  Christopher Nolan",
+  },
+  {
+    title: "Eternal Sunshine of the Spotless Mind",
+    releseDate: " 9 March 2004 ",
+    plot:
+      "When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories.",
+    poster: "https://m.media-amazon.com/images/I/51dPIsyVaxL._AC_.jpg",
+    boxOffice: "$20,000,000",
+    rating: 8.3,
+    director: "Michel Gondry",
+  },
+  {
+    title: "12 Years a Slave",
+    releseDate: "  12 December 2013  ",
+    plot:
+      "In the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
+    poster: "https://m.media-amazon.com/images/I/61QFAKXbb5L._AC_SY879_.jpg",
+    boxOffice: "$20,000,000",
+    rating: 8.1,
+    director: "Steve McQueen",
+  },
+];
